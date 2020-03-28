e8aa9ab8e58de2ba397b1a201fe7163359244ade
Renato Foster <renato.foster@projecthub.com.br>
2020-03-27 20:25:26 -0400

    Config email and update script

--- update.sh
+++ update.sh
@@ -15,9 +15,13 @@ elif [[ "$1" == "update_prod" ]]; then    # used only on server
     git reset
     git checkout .
     git pull --ff-only origin master
+    echo "Copying public/index_prod.php to public/index.php"
     cp public/index_prod.php public/index.php
+    echo "Copying public/.htaccess_prod to public/.htaccess"
     cp public/.htaccess_prod public/.htaccess
+    echo "Copying all public/* to ../public_html/"
     cp -a public/* ../public_html/
+    echo "Pull completed"
 
 else
     if [[ "$1" == "beta" ]]; then
@@ -28,7 +32,7 @@ else
     elif [[ "$1" == "prod" ]]; then
 
         git push origin master
-        ssh u219-fgz2rpsm789q@giow1054.siteground.us -p18765 "source /zwork/update.sh update_prod"
+        ssh u219-fgz2rpsm789q@giow1054.siteground.us -p18765 "source zwork/update.sh update_prod"
 
     else
         echo "Erro: passe 'beta' ou 'prod' como parâmetro"
