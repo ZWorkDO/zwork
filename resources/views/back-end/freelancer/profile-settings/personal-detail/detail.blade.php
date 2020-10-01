d4d45d0a1a65db7288a58686147ebea006ae86ec
Renato Foster <renato.foster@projecthub.com.br>
2020-10-01 10:51:06 -0400

    Fix automatic merge

--- resources/views/back-end/freelancer/profile-settings/personal-detail/detail.blade.php
+++ resources/views/back-end/freelancer/profile-settings/personal-detail/detail.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="wt-tabscontenttitle">
     <h2>{{{ trans('lang.your_details') }}}</h2>
 </div>
@@ -25,32 +24,5 @@
             {!! Form::textarea( 'description', e($description), ['class' =>'form-control', 'placeholder' => trans('lang.ph_desc')] ) !!}
         </div>
     </fieldset>
-=======
-<div class="wt-tabscontenttitle">
-    <h2>{{{ trans('lang.your_details') }}}</h2>
-</div>
-<div class="wt-formtheme">
-    <fieldset>
-        <div class="form-group form-group-half">
-            <span class="wt-select">
-                {!! Form::select( 'gender', ['male' => trans('lang.genders.male'), 'female' => trans('lang.genders.female')], e($gender), ['placeholder' => trans('lang.ph_select_gender')] ) !!}
-            </span>
-        </div>
-        <div class="form-group form-group-half">
-            {!! Form::text( 'first_name', e(Auth::user()->first_name), ['class' =>'form-control', 'placeholder' => trans('lang.ph_first_name')] ) !!}
-        </div>
-        <div class="form-group form-group-half">
-            {!! Form::text( 'last_name', e(Auth::user()->last_name), ['class' =>'form-control', 'placeholder' => trans('lang.ph_last_name')] ) !!}
-        </div>
-        <div class="form-group form-group-half">
-            {!! Form::number( 'hourly_rate', e($hourly_rate), ['class' =>'form-control', 'placeholder' => trans('lang.ph_service_hoyrly_rate')] ) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::text( 'tagline', e($tagline), ['class' =>'form-control', 'placeholder' => trans('lang.ph_add_tagline')] ) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::textarea( 'description', e($description), ['class' =>'form-control', 'placeholder' => trans('lang.ph_desc')] ) !!}
-        </div>
-    </fieldset>
->>>>>>> 002e7d8d0185d58fb9bd541347c9eeaa0d429d94
+
 </div>
