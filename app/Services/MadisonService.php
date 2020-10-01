b518eaecb258ae660e9692ecd091a45803af55a8
Pavel Pichardo <pavelpichardo@gmail.com>
2020-09-30 21:58:45 -0400

    add code to auth madison [madison]

--- app/Services/MadisonService.php
+++ app/Services/MadisonService.php
@@ -2,6 +2,12 @@
 
 namespace App\Services;
 
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp;
+use GuzzleHttp\Client;
+use App\User;
+
 class MadisonService {
 
   public static function createCustomer() {
@@ -14,6 +20,24 @@ class MadisonService {
   }
 
   public static function accessMadison() {
-  }
+    $client = new GuzzleHttp\Client();
+    $res = $client->request('POST', 'http://beta.madisonpos.com/api/v2/auth/sign_in',
+                            ['form_params' => [
+                              'email' => 'demo@madisonpos.com',
+                              'password' => '1234demo'
+                              ]
+                            ]);
+    return $result->getBody(); 
 
+    $clients = json_decode($result, true);
+
+    foreach($clients as $client) {
+        Student::updateOrCreate([
+            'registration_no' => $client->Student Code,
+            'full_name' => $client->Full Name,
+            'student_class' => $client->StudentClass,
+            'facademic_term' => $client->AcademicTerm
+        ]);
+    }              
+  }
 }
